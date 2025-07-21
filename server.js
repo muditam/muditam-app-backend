6a2db0c395755138d613a620c2e5c9f4f70c3010
muditam <madhur.muditam@gmail.com>
2025-07-21 16:03:21 +0530

    app backend updated

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ require('./cron/reminderCron');
 
 const shopifyRoutes = require('./routes/shopify');
 const userRoutes = require('./routes/user');
-const quizRoutes = require('./routes/quiz');
+const quizRoutes = require('./routes/quiz'); 
 const cartRoutes = require('./routes/cart');
 const reminderRoutes = require('./routes/reminder');
-
-const app = express();
+ 
+const app = express(); 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 // MongoDB connection 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true, 
 })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB Error:', err)); 
